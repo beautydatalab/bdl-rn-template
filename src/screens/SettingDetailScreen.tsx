cd8f66a487d2d7206b3d9a9c971b0d48b517d9a1
BDL Macbook Pro(SJL) <seungjoon.lee@beautydatalab.com>
2021-02-10 12:22:08 +0900

    feat: 컴포넌트 구조에서 Container 제거

--- src/screens/SettingDetailScreen.tsx
+++ src/screens/SettingDetailScreen.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
-import SettingDetailScreenContainer from 'src/components/container/SettingDetailScreenContainer';
+import React, {Component} from 'react';
+import {Button} from 'src/components/molecules/buttons/HorizontalButtons';
+import SettingScreenTemplate from 'src/components/templates/SettingScreenTemplate';
 import {SettingScreenProps} from 'src/navigators/types';
 
-const SettingScreen = ({navigation}: SettingScreenProps) => (
-  <SettingDetailScreenContainer navigation={navigation} />
-);
+type Props = {
+  navigation: SettingScreenProps['navigation'];
+};
 
-export default SettingScreen;
+interface State {
+  buttons: Button[];
+}
+
+class SettingDetailScreen extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {buttons: []};
+  }
+  render() {
+    const {buttons} = this.state;
+    return <SettingScreenTemplate title="Setting Screen" buttons={buttons} />;
+  }
+
+  public componentDidMount() {
+    this.fetch();
+  }
+
+  private fetch = () => {
+    const {navigation} = this.props;
+    this.setState({
+      buttons: [
+        {
+          title: '< Prev Screen',
+          onPress: () => navigation.goBack(),
+        },
+        {
+          title: 'console.log ??',
+          onPress: () => console.log('??'),
+        },
+      ],
+    });
+  };
+}
+
+export default SettingDetailScreen;
