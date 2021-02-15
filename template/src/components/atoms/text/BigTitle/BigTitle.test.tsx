bad01ae05a78efedb1eb4546986171e948eb9090
BDL Macbook Pro(SJL) <seungjoon.lee@beautydatalab.com>
2021-02-15 13:39:11 +0900

    feat: test 추가 및 snapshot 추가

---
+++ template/src/components/atoms/text/BigTitle/BigTitle.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {BigTitle} from 'src/components/atoms/text/BigTitle';
+
+test('render correctly', () => {
+  const tree = renderer.create(<BigTitle>BigTitle</BigTitle>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ template/src/components/atoms/text/SectionTitle/SectionTitle.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {SectionTitle} from 'src/components/atoms/text/SectionTitle';
+
+test('render correctly', () => {
+  const tree = renderer.create(<SectionTitle>BigTitle</SectionTitle>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
