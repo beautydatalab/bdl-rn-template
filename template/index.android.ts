fc63d8be8ce31a69d9df696c6040ccd7b8a623bf
BDL Macbook Pro(SJL) <seungjoon.lee@beautydatalab.com>
2021-02-16 18:09:14 +0900

    feat: storybook import 빠진 것 다시 추가

--- template/index.android.ts
+++ template/index.android.ts
@@ -9,6 +9,7 @@ import {AppRegistry} from 'react-native';
 import App from 'src/App';
 
 import {name as appName} from './app.json';
+// import Storybook from './storybook';
 
 if (__DEV__) {
   import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
