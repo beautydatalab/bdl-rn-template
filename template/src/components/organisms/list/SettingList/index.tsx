0603c804381f84477ec7c9e086a6732a90d0b4c9
BDL Macbook Pro(SJL) <seungjoon.lee@beautydatalab.com>
2021-02-19 17:51:07 +0900

    feat: SettingList 에서 stylesheet 제거

--- template/src/components/organisms/list/SettingList/index.tsx
+++ template/src/components/organisms/list/SettingList/index.tsx
@@ -4,8 +4,11 @@ import {
   FlatList,
   FlatListProps,
   ListRenderItem,
-  StyleSheet,
+  ViewStyle,
+  StyleProp,
 } from 'react-native';
+
+import styled from 'styled-components/native';
 
 import {ListItem} from 'src/components/molecules/listItem/ListItem';
 import {margins} from 'src/styles/space';
@@ -20,12 +23,22 @@ interface Props
   settings: Setting[];
 }
 
+const List = styled(FlatList as new () => FlatList<Setting>).attrs({
+  contentContainerStyle: {marginBottom: margins.tiny} as StyleProp<ViewStyle>,
+})`
+  width: 100%;
+`;
+
+const Title = styled(ListItem.Title)`
+  margin-bottom: ${margins.tiny}px;
+`;
+
 const renderItem: ListRenderItem<Setting> = ({item}) => {
   const {title, description} = item;
   return (
     <ListItem bottomDivider chevron>
       <ListItem.Content>
-        <ListItem.Title style={styles.title}>{title}</ListItem.Title>
+        <Title>{title}</Title>
         <ListItem.SubTitle>{description}</ListItem.SubTitle>
       </ListItem.Content>
     </ListItem>
@@ -36,10 +49,9 @@ const keyExtractor = (_item: Setting, index: number) => {
   return index.toString();
 };
 
-export const SettingList = ({settings, style, ...rest}: Props) => {
+export const SettingList = ({settings, ...rest}: Props) => {
   return (
-    <FlatList<Setting>
-      style={[styles.list, style]}
+    <List
       renderItem={renderItem}
       data={settings}
       keyExtractor={keyExtractor}
@@ -48,12 +60,3 @@ export const SettingList = ({settings, style, ...rest}: Props) => {
     />
   );
 };
-
-const styles = StyleSheet.create({
-  list: {
-    width: '100%',
-  },
-  title: {
-    marginBottom: margins.tiny,
-  },
-});
