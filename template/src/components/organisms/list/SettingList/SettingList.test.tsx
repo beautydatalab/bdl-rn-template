c11ecb89cde6099ff596c84fd9c11d94d8cdb981
BDL Macbook Pro(SJL) <seungjoon.lee@beautydatalab.com>
2021-02-19 11:58:43 +0900

    feat: SettingList 컴포넌트 storybook 및 test 추가

---
+++ template/src/components/organisms/list/SettingList/SettingList.test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import renderer from 'react-test-renderer';
+
+import {SettingList} from './index';
+
+const settings = [
+  {
+    title: '로그인',
+    description:
+      '현재 로그인이 되어있지 않아요. 로그인해서 더 많은 서비스를 누려보아요',
+  },
+  {
+    title: '알림 설정',
+    description: '알림 설정을 통해서 앱의 알림을 통제할 수 있습니다',
+  },
+  {
+    title: '테마 변경',
+    description:
+      '앱이 마음에 안드세요? 그러면 테마를 바꿔서 기분을 내보는 것은 어떠세요?',
+  },
+  {title: '회원탈퇴', description: '너무 아쉽지만 ㅠ...  보내줄게요'},
+];
+
+test('render correctly', () => {
+  const tree = renderer
+    .create(
+      <>
+        <SettingList settings={settings} />
+      </>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- template/storybook/storyLoader.js
+++ template/storybook/storyLoader.js
@@ -7,12 +7,14 @@ function loadStories() {
   require('../src/components/atoms/button/button.stories');
   require('../src/components/atoms/text/text.stories');
   require('../src/components/molecules/listItem/listItem.stories');
+  require('../src/components/organisms/list/list.stories');
 }
 
 const stories = [
   '../src/components/atoms/button/button.stories',
   '../src/components/atoms/text/text.stories',
   '../src/components/molecules/listItem/listItem.stories',
+  '../src/components/organisms/list/list.stories',
 ];
 
 module.exports = {
